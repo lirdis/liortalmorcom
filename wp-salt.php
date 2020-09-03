6cc79ec95038c9d5349d96e5ee134790d50ed754
Lior Talmor <lior@liortalmor.com>
2020-09-03 00:14:33 +0000

    1st

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         '8pL8bFtHK4WMNPTgsImJrPfc76piJVBCB96QhvwFVF2NCKDudiIJ46TpMjGN45Vo');
+define('SECURE_AUTH_KEY',  'HLgDpa8Wg7hgvFITNvCmy01U3zqcTogXzojihPgsqB93a5PdpXxRvCfqAeTLPP8N');
+define('LOGGED_IN_KEY',    'tioKHbjrJw0y4ba4NIrxHPFsa797Im8EPTQye565wjA31D6yvqJGCxV1F9KIvQSa');
+define('NONCE_KEY',        '1vKS1IuSQd1D5KhGXtdwgjCJuViG1KCJIgCawWsFbaYSrwBzjzYEfyL3KzXuIumx');
+define('AUTH_SALT',        'nBQoWixWNtaRErvIQQBv0LCL7RL7DL57jRWt0tbVEDib0vsQrAUiRrFedes21yQa');
+define('SECURE_AUTH_SALT', 'u6PARAgDczEJqLq8eSH0ryU4CDdi9EPF321aBff9xhht42WKvX7gD30PrdzW8Kwh');
+define('LOGGED_IN_SALT',   'iY8amGLq9FbvJtU84zGiDEEf00Bi1JxyQueE8SrJwVHCVhtcvo9qrs0urbPt8Cj0');
+define('NONCE_SALT',       'rrQrmK9cjh1HC8QmectAQNIX4NCwvNhfCjr4FhCx5igza2Sn6m7zu0f7IjrtnxJp');
